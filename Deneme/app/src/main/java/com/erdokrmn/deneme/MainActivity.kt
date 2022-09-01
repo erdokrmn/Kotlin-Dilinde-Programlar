bc17755323423e6895a83728a645f3f7c93be7cc
erdokrmn <erdokrmn@hotmail.com>
2022-09-01 16:23:24 +0300

    First Commit

---
+++ Deneme/app/src/main/java/com/erdokrmn/deneme/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.erdokrmn.deneme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun deneme(view: View){
+        println("deneme123")
+
+    }
+}
